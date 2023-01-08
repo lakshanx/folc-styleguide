18175ae53c2db50b2f4e5c9daf57d4fa810b7b4c
Lakshan Danushka <lakshanx@gmail.com>
2023-01-09 03:54:01 +0530

    improve dropdowns

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,8 @@
 $(function () {
 	$('[data-toggle="popover"]').popover();
 });
+
+// Stop auto closing of dropdowns with CSS class .avoid-auto-close
+$(document).on('click', '.avoid-auto-close.dropdown-menu', function (e) {
+	e.stopPropagation();
+});
