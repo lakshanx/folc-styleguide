088367e252eb68a49add838e81490874831df175
Lakshan Danushka <lakshanx@gmail.com>
2023-01-20 18:06:55 +0530

    categories tabs - mobile

--- src/js/modules/categories-tabs.js
+++ src/js/modules/categories-tabs.js
@@ -6,4 +6,28 @@ $(document).ready(function() {
         });
         $('#categories-tabs-content').addClass('bg-' + className.split('--')[1]); // add the class "bg-[className]" to #categories-tabs-content
     });
+
+        var select = $('#select-categories');
+        select.addClass('categories-select');
+        
+        // Loop through the existing nav-item elements to create new option elements
+        $('.categories-tabs .nav-item').each(function() {
+            var navItem = $(this);
+            var option = $('<option>');
+            option.val(navItem.find('a').attr('href'));
+            option.text(navItem.find('a').text());
+            select.append(option);
+        });
+        
+        // When the select element is clicked
+        select.on('change', function() {
+            var selectedTab = $(this).val();
+            var selectedContent = $(selectedTab).find('.categories-cols').html();
+
+            // Append the relevant content from the .tab-pane .categories-cols element below the select element
+            $('#categories-select-content').html(selectedContent);
+            $('#categories-select-content a').addClass("with-prefix pb-1");
+
+        });
+
 });
