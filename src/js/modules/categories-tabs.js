ff5c03b2bfd3383aad01de77b3cc660c36adacd8
Lakshan Danushka <lakshanx@gmail.com>
2023-01-24 03:39:22 +0530

    fix eslint

--- src/js/modules/categories-tabs.js
+++ src/js/modules/categories-tabs.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
 	$('.categories-tabs .nav-link').click(function () {
 		const className = $(this).attr('class').split(' ')[1]; // get the class that starts with "nav-link--"
-		$('#categories-tabs-content').removeClass(function (index, className) {
-			return (className.match(/(^|\s)bg-\S+/g) || []).join(' '); // remove any classes that start with "bg-"
+		$('#categories-tabs-content').removeClass(function (index, bgClassName) {
+			return (bgClassName.match(/(^|\s)bg-\S+/g) || []).join(' '); // remove any classes that start with "bg-"
 		});
 		$('#categories-tabs-content').addClass(`bg-${className.split('--')[1]}`); // add the class "bg-[className]" to #categories-tabs-content
 	});
