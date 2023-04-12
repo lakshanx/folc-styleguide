fe00d8d44a35522c5c8ac4ed0c996a6afe983552
Lakshan Danushka <lakshanx@gmail.com>
2023-04-12 16:59:12 +0530

    Country header select functionality -- work on both

--- src/js/modules/country-header.js
+++ src/js/modules/country-header.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-// Function to handle the change event
+	// Function to handle the change event
 	function handleChange() {
 		const url = $(this).val();
 		if (url) {
@@ -7,20 +7,15 @@ $(document).ready(function () {
 		}
 	}
 
-	// Function to initialize and customize the duplicated select element
-	function initializeDuplicatedSelect($select) {
-		$select.removeClass('text-uppercase text-gray mt-2')
-			.addClass('d-md-none mb-2')
-			.on('change', handleChange);
-	}
-
 	// Get the original .select-region element
 	const $originalSelectRegion = $('.select-region');
 
-	// Clone the .select-region element and initialize the duplicated element
-	const $duplicatedSelectRegion = $originalSelectRegion.clone();
-	initializeDuplicatedSelect($duplicatedSelectRegion);
+	// Clone the .select-region element and insert the duplicated element before the .categories-form element
+	const $duplicatedSelectRegion = $originalSelectRegion.clone()
+		.removeClass('text-uppercase text-gray mt-2')
+		.addClass('d-md-none mb-2')
+		.insertBefore('.categories-form');
 
-	// Insert the duplicated .select-region element before the .categories-form element
-	$duplicatedSelectRegion.insertBefore('.categories-form');
+	// Attach the handleChange function to both the original and duplicated select elements
+	$originalSelectRegion.add($duplicatedSelectRegion).on('change', handleChange);
 });
