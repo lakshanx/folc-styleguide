815af83076b1f492a04ace1b721eab8f23e86d3e
Lakshan Danushka <lakshanx@gmail.com>
2023-04-12 16:33:43 +0530

    categories tabs --  add colors to select element etc

--- src/js/modules/categories-tab.js
+++ src/js/modules/categories-tab.js
@@ -1,3 +1,16 @@
+// Function to update the bg-* class and text-white class of the select element
+function updateSelectBgClass($select) {
+	const selectedTab = $select.val();
+	if (selectedTab && selectedTab.includes('-')) {
+		const bgClass = `bg-${selectedTab.split('-')[1]}`;
+		$select.removeClass(function (index, className) {
+			return (className.match(/(^|\s)(bg-\S+|text-white)/g) || []).join(' ');
+		});
+		$select.addClass(bgClass);
+		$select.addClass('text-white arrow-white');
+	}
+}
+
 $(document).ready(function () {
 	const $categoriesTabs = $('.categories-tab');
 
@@ -6,6 +19,7 @@ $(document).ready(function () {
 		const $select = $categoriesTabs.find('.select-categories');
 		const $categoriesSelectContent = $categoriesTabs.find('.categories-select-content');
 
+		// When a category tab is clicked
 		$('.categories-tab .nav-link').click(function () {
 			const className = $(this).attr('class').split(' ')[1];
 			$categoriesTabContent.removeClass(function (index, bgClassName) {
@@ -14,6 +28,7 @@ $(document).ready(function () {
 			$categoriesTabContent.addClass(`bg-${className.split('--')[1]}`);
 		});
 
+		// Adding options to the select element
 		$('.categories-tab .nav-item').each(function () {
 			const $navItem = $(this);
 			const $option = $('<option>');
@@ -22,11 +37,16 @@ $(document).ready(function () {
 			$select.append($option);
 		});
 
+		// When the select element is changed
 		$select.on('change', function () {
 			const selectedTab = $(this).val();
 			const selectedContent = $(selectedTab).find('.categories-cols').html();
 			$categoriesSelectContent.html(selectedContent);
 			$categoriesSelectContent.find('a').addClass('with-prefix pb-1');
+			updateSelectBgClass($(this)); // Update the bg-* and text-white classes
 		});
+
+		// Set the initial bg-* and text-white classes for the select element
+		updateSelectBgClass($select);
 	}
 });
