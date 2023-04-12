dd42f2613211cfaf091e35e8dcf0ba893cf2604b
Lakshan Danushka <lakshanx@gmail.com>
2023-04-12 16:33:09 +0530

    country header improvements

--- src/js/modules/country-header.js
+++ src/js/modules/country-header.js
@@ -1,8 +1,26 @@
-$(document).ready(function () {
-	$('.country-header select').change(function () {
+$(document).ready(function() {
+// Function to handle the change event
+	function handleChange() {
 		const url = $(this).val();
 		if (url) {
 			window.location.href = url;
 		}
-	});
+	}
+
+	// Function to initialize and customize the duplicated select element
+	function initializeDuplicatedSelect($select) {
+		$select.removeClass('text-uppercase text-gray mt-2')
+			.addClass('d-md-none mb-2')
+			.on('change', handleChange);
+	}
+
+	// Get the original .select-region element
+	const $originalSelectRegion = $('.select-region');
+
+	// Clone the .select-region element and initialize the duplicated element
+	const $duplicatedSelectRegion = $originalSelectRegion.clone();
+	initializeDuplicatedSelect($duplicatedSelectRegion);
+
+	// Insert the duplicated .select-region element before the .categories-form element
+	$duplicatedSelectRegion.insertBefore('.categories-form');
 });
